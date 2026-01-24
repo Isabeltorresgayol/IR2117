348b7422b4ab78d16ba70de12cea7f2b783003a6
Isabel <isabel@al435576.uji.es>
2026-01-24 18:41:05 +0100

    V1

---
+++ Sum/sum.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int number;
+    int sum = 0;
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    std::cout << "You entered: " << number << std::endl;
+
+    return 0;
+}
+
