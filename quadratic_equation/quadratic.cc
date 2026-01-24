f719f9198435b496c3ea5c2f88dda4708cadee06
Isabel <isabel@al435576.uji.es>
2026-01-24 20:22:48 +0100

    V1(quadratic) Pedir coeficientes, asumir soluciones = 0, imprimir soluciones

---
+++ quadratic_equation/quadratic.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cout << "Ingrese a, b y c: ";
+    cin >> a >> b >> c;
+
+    double x1 = 0, x2 = 0;
+    cout << "Soluciones: x1 = " << x1 << ", x2 = " << x2 << endl;
+
+    return 0;
+}
+
