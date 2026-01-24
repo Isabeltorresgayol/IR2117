d05c2035450ca6ba101d7627576f440bc23f859d
Isabel <isabel@al435576.uji.es>
2026-01-24 19:05:29 +0100

    V4 sum_ab

--- Sum/sum_ab.cc
+++ Sum/sum_ab.cc
@@ -10,15 +10,15 @@ int main() {
     } while (a < 1);
 
     do {
-        std::cout << "Enter second number (>=1): ";
+        std::cout << "Enter second number (>=1 and >= first number): ";
         std::cin >> b;
-    } while (b < 1);
+    } while (b < 1 || b < a);
 
     for (int i = a; i <= b; i++) {
         sum += i;
     }
 
-    std::cout << "Sum = " << sum << std::endl;
+    std::cout << "Sum from " << a << " to " << b << " is: " << sum << std::endl;
 
     return 0;
 }
