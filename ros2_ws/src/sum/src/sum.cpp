63244df0be44394709835f1cd908cf8788dfbcb9
Isabel <isabel@al435576.uji.es>
2026-02-07 12:18:22 +0100

    Versión 4 de sum.cpp (ya suma)

--- ros2_ws/src/sum/src/sum.cpp
+++ ros2_ws/src/sum/src/sum.cpp
@@ -2,21 +2,24 @@
 #include "std_msgs/msg/int32.hpp"
 #include <iostream>
 
+int sum;
+
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
-    std::cout << msg->data << std::endl;
+	sum += msg->data;
+	std::cout << sum << std::endl;
 }
 
 int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("sum");
-    auto subscription =
-        node->create_subscription<std_msgs::msg::Int32>(
-            "number", 10, topic_callback);
+	sum = 0;
+	rclcpp::init(argc, argv);
+	auto node = rclcpp::Node::make_shared("sum");
+	auto subscription =
+        	node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
 
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+	rclcpp::spin(node);
+	rclcpp::shutdown();
+	return 0;
 }
 
