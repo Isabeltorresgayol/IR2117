3f4f4a32d53cecde2f2dee63beb4acf897f78414
Isabel <isabel@al435576.uji.es>
2026-02-10 11:52:38 +0100

    cambiado en mean.cc el nombre sum -->mean

--- ros2_ws/src/stats/src/mean.cc
+++ ros2_ws/src/stats/src/mean.cc
@@ -18,11 +18,11 @@ int main(int argc, char * argv[])
 {
 	sum = 0;
 	rclcpp::init(argc, argv);
-	auto node = rclcpp::Node::make_shared("sum");
+	auto node = rclcpp::Node::make_shared("mean");
 	auto subscription =
         	node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
 
-	publisher = node->create_publisher<std_msgs::msg::Int32>("sum", 10);
+	publisher = node->create_publisher<std_msgs::msg::Int32>("mean", 10);
 	rclcpp::spin(node);
 	rclcpp::shutdown();
 	return 0;
