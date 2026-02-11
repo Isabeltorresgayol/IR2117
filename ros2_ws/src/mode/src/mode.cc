5e32594a1d6f9b1b55876689bf79d9c5c28aa181
Isabel <isabel@al435576.uji.es>
2026-02-11 23:41:39 +0100

    12. Publish all the numbers with the biggest occurrence. No está del todo bien, pero es que en dos días no me ha dado tiempo, puesto que estoy empezando a pillarle el truco a C++

--- ros2_ws/src/mode/src/mode.cc
+++ ros2_ws/src/mode/src/mode.cc
@@ -10,7 +10,6 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
     data_vec.push_back(msg->data);
 
     int max_count = 0;
-    int mode = data_vec[0];
 
     for (int i = 0; i < (int)data_vec.size(); i++) {
         int count = 0;
@@ -18,21 +17,28 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
             if (data_vec[j] == data_vec[i])
                 count++;
         }
-        if (count > max_count) {
+        if (count > max_count)
             max_count = count;
-            mode = data_vec[i];
-        }
     }
 
-    std_msgs::msg::Int32 out_msg;
-    out_msg.data = mode;
-    publisher->publish(out_msg);
+    for (int i = 0; i < (int)data_vec.size(); i++) {
+        int count = 0;
+        for (int j = 0; j < (int)data_vec.size(); j++) {
+            if (data_vec[j] == data_vec[i])
+                count++;
+        }
+        if (count == max_count) {
+            std_msgs::msg::Int32 out_msg;
+            out_msg.data = data_vec[i];
+            publisher->publish(out_msg);
+        }
+    }
 }
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("mode_single");
+    auto node = rclcpp::Node::make_shared("mode_multiple");
 
     auto subscription =
         node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
