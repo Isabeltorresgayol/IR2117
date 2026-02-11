a41299da00fd6d68d1c50bff353e46e7e4b24c4d
Isabel <isabel@al435576.uji.es>
2026-02-11 22:47:20 +0100

    6. Store a vector of the input topics and display it

--- ros2_ws/src/median/src/mediana.cc
+++ ros2_ws/src/median/src/mediana.cc
@@ -1,33 +1,30 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
-#include "std_msgs/msg/float64.hpp"
+#include <vector>
+#include <iostream>
 
-double sum;
-int count;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float64>> publisher;
+std::vector<int> data_vec;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
-    sum += msg->data;
-    count++;
+    
+    data_vec.push_back(msg->data);
 
-    std_msgs::msg::Float64 out_msg;
-    out_msg.data = sum / count;
-    publisher->publish(out_msg);
+    
+    std::cout << "Vector: ";
+    for (int x : data_vec) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main(int argc, char * argv[])
 {
-    sum = 0.0;
-    count = 0;
-
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("mean");
+    auto node = rclcpp::Node::make_shared("vector");
 
     auto subscription =
         node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-
-    publisher = node->create_publisher<std_msgs::msg::Float64>("mean", 10);
 
     rclcpp::spin(node);
     rclcpp::shutdown();
