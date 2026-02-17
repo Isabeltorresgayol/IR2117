6d33f6335d90b9b3425920bd144f7f5c783140fc
Isabel <isabel@al435576.uji.es>
2026-02-17 22:29:29 +0100

    Move forward 1m and turn 90º

--- tb3_ws/src/robot_trayectory/src/square.cpp
+++ tb3_ws/src/robot_trayectory/src/square.cpp
@@ -16,26 +16,39 @@ int main(int argc, char * argv[])
         node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
     geometry_msgs::msg::Twist message;
+
+    
     rclcpp::WallRate loop_rate(10ms);
 
     int i = 0;
-    int n = 1000;   // numero de iteraciones para 1 metro
+    int n = 1000;   // iteraciones para 1 metro
 
-    while (rclcpp::ok() && i < n) {
+    
+    while (rclcpp::ok() && (i < n)) {
         i++;
 
-        // Velocidad lineal en x
         message.linear.x = 0.1;
-
-        // Sin giro
         message.angular.z = 0.0;
 
         publisher->publish(message);
-
         rclcpp::spin_some(node);
         loop_rate.sleep();
     }
-    
+
+    i = 0;
+    n = 1000;   // iteraciones para 90 grados
+
+    while (rclcpp::ok() && (i < n)) {
+        i++;
+
+        message.linear.x = 0.0;
+        message.angular.z = 0.157;  // aprox 9 grados en rad/s
+
+        publisher->publish(message);
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
+    }
+
     
     message.linear.x = 0.0;
     message.angular.z = 0.0;
