1a14517b23c16339a5fbdab596e29fe765e51346
Isabel <isabel@al435576.uji.es>
2026-03-20 23:12:38 +0100

    V5. SE MUEVE EL ROBOT Y SE PARA ANTES DE LA PARED!!

--- tb3_ws/src/robot_trayectory/src/wandering.cpp
+++ tb3_ws/src/robot_trayectory/src/wandering.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
+#include <iostream>
+#include <algorithm> // std::min_element
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 
@@ -14,23 +15,25 @@ int main(int argc, char * argv[])
 
     auto publisher =
         node->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
-        
-    // Subscriber a /scan (callback vacío de momento)
+
+    //V5: variable para guardar la mínima distancia detectada
+    float min_distance = std::numeric_limits<float>::infinity();
+
+    // Subscriber a /scan
     auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>(
         "/scan", 10,
-        [](const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        [&](const sensor_msgs::msg::LaserScan::SharedPtr msg) { 
             auto n = msg->ranges.size();
-		if (n >= 360) {
-                
+            if (n >= 360) {
                 std::vector<float> front_back;
                 front_back.insert(front_back.end(), msg->ranges.begin(), msg->ranges.begin() + 10);
                 front_back.insert(front_back.end(), msg->ranges.begin() + 350, msg->ranges.begin() + 360);
 
-                // Calcular mínimo
-                float min_range = *std::min_element(front_back.begin(), front_back.end());
+                
+                min_distance = *std::min_element(front_back.begin(), front_back.end());
 
-                // Mostrar en pantalla
-                std::cout << "Mínimo distancia (0..9 + 350..359): " << min_range << std::endl;
+                
+                std::cout << "Mínimo distancia (0..9 + 350..359): " << min_distance << std::endl; 
                 std::cout << "------------------------" << std::endl;
             }
         }
@@ -44,10 +47,17 @@ int main(int argc, char * argv[])
     rclcpp::WallRate loop_rate(10ms);
 
     while (rclcpp::ok()) {
+        
+        if (min_distance >= 1.0) {       //si hay espacio, avanza
+            message.linear.x = 0.2;      //velocidad hacia delante
+            message.angular.z = 0.0;
+        } else {                         //si obstáculo a <1m, para
+            message.linear.x = 0.0;
+            message.angular.z = 0.0;
+        }
+
         publisher->publish(message);
-
         rclcpp::spin_some(node);
-
         loop_rate.sleep();
     }
 
