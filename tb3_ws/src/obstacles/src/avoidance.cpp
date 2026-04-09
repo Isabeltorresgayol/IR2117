adb2a2dc51c2ecdc9ce85c1f441a77962df595f6
Isabel <isabel@al435576.uji.es>
2026-04-09 14:20:07 +0200

    V1. Add subscribers to: a) /front/obstacle, b)/right/obstacle, c)/left/obstacle

--- tb3_ws/src/obstacles/src/avoidance.cpp
+++ tb3_ws/src/obstacles/src/avoidance.cpp
@@ -1,8 +1,17 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "example_interfaces/msg/bool.hpp"
+
 
 using namespace std::chrono_literals;
+
+//Callbacks vacíos by the moment
+void callback_front(const example_interfaces::msg::Bool::SharedPtr msg) {}
+
+void callback_left(const example_interfaces::msg::Bool::SharedPtr msg) {}
+
+void callback_right(const example_interfaces::msg::Bool::SharedPtr msg) {}
 
 int main(int argc, char * argv[])
 {
@@ -10,6 +19,16 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("avoidance");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   
+   // Subscribers
+  auto subs_front = node->create_subscription<example_interfaces::msg::Bool>(
+    "/front/obstacle", 10, callback_front);
+
+  auto subs_left = node->create_subscription<example_interfaces::msg::Bool>(
+    "/left/obstacle", 10, callback_left);
+
+  auto subs_right = node->create_subscription<example_interfaces::msg::Bool>(
+    "/right/obstacle", 10, callback_right);
+
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(50ms);
 
