7674ee85fcc9fa50df34b2b2de0167777945d2c2
Isabel <isabel@al435576.uji.es>
2026-04-16 15:33:32 +0200

    V2.Equilateral. He añadido los parámetros: angular_speed,linear_speed,side_length. Pero aún no describo ningún girlo, sigue en linea recta

--- ros2_ws/src/triangles/src/equilateral.cpp
+++ ros2_ws/src/triangles/src/equilateral.cpp
@@ -8,6 +8,15 @@ int main(int argc, char * argv[])
     rclcpp::init(argc, argv);
     
     auto node = rclcpp::Node::make_shared("equilateral");
+    
+    //añadimos los parámetros
+    node->declare_parameter("angular_speed", 0.5);
+    node->declare_parameter("linear_speed", 1.0);
+    node->declare_parameter("side_length", 2.0);
+
+    double angular_speed=node->get_parameter("angular_speed").as_double();
+    double linear_speed=node->get_parameter("linear_speed").as_double();
+    double side_length=node->get_parameter("side_length").as_double();
 
     auto publisher =
         node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
@@ -17,6 +26,8 @@ int main(int argc, char * argv[])
     rclcpp::WallRate loop_rate(10ms);
 
     while (rclcpp::ok()) {
+    
+    	//aún seguimos rectos
         message.linear.x = 1.0;
         message.angular.z = 0.0;
 
