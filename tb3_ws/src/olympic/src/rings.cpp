b61ca11e199adf5a1268dc0283aeb4b97bf8ad7b
Isabel <isabel@al435576.uji.es>
2026-04-23 12:19:35 +0200

    VERSIÓN 5.1 SIMPLE: " Add the “radius” parameter

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -1,6 +1,5 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
@@ -9,13 +8,22 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("rings");
+
+  // Declaramos el radio
+  node->declare_parameter("radius", 1.0);
+
+  // Obtenemos el valor del parámetro
+  double radius = node->get_parameter("radius").as_double();
+
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(500ms);
 
+  double linear_speed = 1.0;
+
   while (rclcpp::ok()) {
-    message.linear.x = 1.0;   // velocidad hacia delante
-    message.angular.z = 1.0;  // velocidad de giro 
+    message.linear.x = linear_speed;
+    message.angular.z = linear_speed / radius;  // usamos el radio
 
     publisher->publish(message);
     rclcpp::spin_some(node);
