962fe243f2a0dd6ee35ee3b17c4485bc537342c6
Isabel <isabel@al435576.uji.es>
2026-05-02 15:41:42 +0200

    PASO 4,5,6:Realizado un aro correctamente. He cambiado el main y he añadido el geometry

--- ros2_ws/src/olympic/src/rings_action_server.cpp
+++ ros2_ws/src/olympic/src/rings_action_server.cpp
@@ -4,9 +4,13 @@
 #include "olympic_interfaces/action/rings.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
+#include "geometry_msgs/msg/twist.hpp" //añadido en el PASO 6
 
 using Rings = olympic_interfaces::action::Rings;
 using GoalHandleRings = rclcpp_action::ServerGoalHandle<Rings>;
+
+
+//---------------------------------------------------------------------------
 
 // Callback para nuevos goals
 rclcpp_action::GoalResponse handle_goal(
@@ -29,29 +33,38 @@ rclcpp_action::CancelResponse handle_cancel(
 }
 
 // Declaración adelantada
-void execute(const std::shared_ptr<GoalHandleRings> goal_handle){
-
+void execute(const std::shared_ptr<GoalHandleRings> goal_handle, rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher){
+  //Se ha añadido: rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher PASO 6
   auto feedback = std::make_shared<Rings::Feedback>();
   auto result = std::make_shared<Rings::Result>();
 
   RCLCPP_INFO(
     rclcpp::get_logger("rings_action_server"),
     "Executing goal");
+    geometry_msgs::msg::Twist msg;
 
-  for (int i = 0; i < 5; i++)
-  {
-    feedback->drawing_ring = i;
-    feedback->ring_angle = i * 10.0;
+   // velocidad → círculo
+   msg.linear.x = 2.0;
+   msg.angular.z = 2.0;
 
+   // duración del círculo (~6 segundos)
+   for (int i = 0; i < 100; i++)
+   {
+    publisher->publish(msg);
+
+    feedback->drawing_ring = 0;
+    feedback->ring_angle = i * 3.6; // aprox 360 grados
+ 
     goal_handle->publish_feedback(feedback);
 
-    rclcpp::sleep_for(std::chrono::milliseconds(500));
-  }
+    rclcpp::sleep_for(std::chrono::milliseconds(60));
+   }
+  
 
-  result->rings_completed = 5;
-  goal_handle->succeed(result);
+   result->rings_completed = 5;
+   goal_handle->succeed(result);
 
-  RCLCPP_INFO(
+   RCLCPP_INFO(
     rclcpp::get_logger("rings_action_server"),
     "Goal succeeded");
 
@@ -60,10 +73,10 @@ void execute(const std::shared_ptr<GoalHandleRings> goal_handle){
 
 // Callback cuando se acepta un goal
 void handle_accepted(
-  const std::shared_ptr<GoalHandleRings> goal_handle)
+  const std::shared_ptr<GoalHandleRings> goal_handle, rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher)
 {
-  std::thread{execute, goal_handle}.detach();
-}
+  std::thread{execute, goal_handle, publisher}.detach();
+} //Se ha añadido el publisher PASO 6
 
 
 
@@ -74,16 +87,21 @@ int main(int argc, char ** argv)
 
   auto node = rclcpp::Node::make_shared("rings_action_server");
 
+  auto publisher =
+    node->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+
   auto action_server =
     rclcpp_action::create_server<Rings>(
       node,
       "rings",
       handle_goal,
       handle_cancel,
-      handle_accepted);
+      std::bind(&handle_accepted, std::placeholders::_1, publisher)
+    );
 
   rclcpp::spin(node);
   rclcpp::shutdown();
 
   return 0;
 }
+
