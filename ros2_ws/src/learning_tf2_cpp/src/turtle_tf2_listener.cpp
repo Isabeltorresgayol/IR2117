4a5e04b6c8e486f58d2f7be3defe0ffd9e77b518
Isabel <isabel@al435576.uji.es>
2026-05-07 20:13:10 +0200

    PASO 3: Modificar el listener, refactorizar el listener para usar una estructura basada en clases y temporizadores (timers).

--- ros2_ws/src/learning_tf2_cpp/src/turtle_tf2_listener.cpp
+++ ros2_ws/src/learning_tf2_cpp/src/turtle_tf2_listener.cpp
@@ -1,69 +1,61 @@
 #include <chrono>
+#include <memory>
 #include <string>
-#include <cmath>
 
-#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "tf2/exceptions.h"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 
 using namespace std::chrono_literals;
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("turtle_tf2_frame_listener");
+class TurtleFollower : public rclcpp::Node {
+public:
+  TurtleFollower() : Node("turtle_tf2_frame_listener") {
+    // Declarar parámetros
+    target_frame_ = this->declare_parameter<std::string>("target_frame", "turtle1");
+    turtle_name_ = this->declare_parameter<std::string>("turtlename", "turtle2");
 
-  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>(
-    "turtle2/cmd_vel", 1);
+    // Inicializar TF
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-  auto target_frame =
-    node->declare_parameter<std::string>("target_frame", "turtle1");
+    // Publicador
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
+      turtle_name_ + "/cmd_vel", 1);
 
-  auto tf_buffer =
-    std::make_unique<tf2_ros::Buffer>(node->get_clock());
-
-  auto tf_listener =
-    std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
-
-  rclcpp::WallRate loop_rate(1s);
-
-  geometry_msgs::msg::TransformStamped t;
-  geometry_msgs::msg::Twist msg;
-
-  std::string fromFrameRel = target_frame.c_str();
-  std::string toFrameRel = "turtle2";
-
-  const double scaleRotationRate = 1.0;
-  const double scaleForwardSpeed = 0.5;
-
-  while (rclcpp::ok()) {
-    try {
-      t = tf_buffer->lookupTransform(
-        toFrameRel, fromFrameRel,
-        tf2::TimePointZero);
-    } catch (const tf2::TransformException & ex) {
-      RCLCPP_INFO(
-        node->get_logger(), "Could not transform %s to %s: %s",
-        toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
-    }
-
-    msg.angular.z = scaleRotationRate * atan2(
-      t.transform.translation.y,
-      t.transform.translation.x);
-
-    msg.linear.x = scaleForwardSpeed * sqrt(
-      pow(t.transform.translation.x, 2) +
-      pow(t.transform.translation.y, 2));
-
-    publisher->publish(msg);
-
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
+    // Timer: ejecuta la lógica cada 100ms
+    timer_ = this->create_wall_timer(100ms, std::bind(&TurtleFollower::on_timer, this));
   }
 
+private:
+  void on_timer() {
+    geometry_msgs::msg::TransformStamped t;
+
+    try {
+      t = tf_buffer_->lookupTransform(turtle_name_, target_frame_, tf2::TimePointZero);
+    } catch (const tf2::TransformException & ex) {
+      return; // Esperar a que el frame esté disponible
+    }
+
+    geometry_msgs::msg::Twist msg;
+    msg.angular.z = 1.0 * atan2(t.transform.translation.y, t.transform.translation.x);
+    msg.linear.x = 0.5 * sqrt(pow(t.transform.translation.x, 2) + pow(t.transform.translation.y, 2));
+
+    publisher_->publish(msg);
+  }
+
+  std::string target_frame_;
+  std::string turtle_name_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TurtleFollower>());
   rclcpp::shutdown();
   return 0;
 }
